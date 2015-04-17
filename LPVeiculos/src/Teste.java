96c461a27aa38d0508b861d887630268cce0383b
Carlos Renato <carlos.asantos@al.infnet.edu.br>
2015-04-17 20:05:18 -0300

    Printando na Main
    teste via Desktop

--- LPVeiculos/src/Teste.java
+++ LPVeiculos/src/Teste.java
@@ -3,6 +3,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Carlos Renato está conectado via Github Desktop.");
 
 	}
 
