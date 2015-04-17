266919f7b3545b2b2ade95d5bfece8f36b8222c7
Carlos Renato <carlos.asantos@al.infnet.edu.br>
2015-04-17 20:12:54 -0300

    Update Teste.java
    testando pull

--- LPVeiculos/src/Teste.java
+++ LPVeiculos/src/Teste.java
@@ -4,7 +4,8 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mais um testeziin");
-		System.out.println("Carlos Renato est· conectado via Github Desktop.");
+		System.out.println("Carlos Renato est√° conectado via Github Desktop.");
+		//Teste de pull
 
 	}
 
