6003f51aa454936bc5073f2124219c876d9c8abb
Carlos Renato <carlos.asantos@al.infnet.edu.br>
2015-04-23 17:44:29 -0300

    update

--- LPVeiculos/src/model/veiculos/caracteristicas/carro/CategoriasCarros.java
+++ LPVeiculos/src/model/veiculos/caracteristicas/carro/CategoriasCarros.java
@@ -7,6 +7,21 @@ package model.veiculos.caracteristicas.carro;
  *
  */
 public enum CategoriasCarros {
+	ENTRADA("Entrada"), HATCHPEQUENO("Hatch Pequeno"), HATCHMEDIO ("Hatch Médio"),
+	SEDANPEQUENO("Sedan Pequeno"), SEDANCOMPACTO ("Sedan Compacto"), SEDANMEDIO("Sedan Médio"),
+	SEDANGRANDE ("Sedan Grande"), SWMEDIO("SW Médio"), SWGRANDE ("SW Grande"),
+	MONOCAB ("Monocab"), GRANDCAB ("Grandcab"), SPORT("Sport"),
+	PICKUPPEPEQUENA("Pick-up Pequena"), PICKUPPEGRANDE("Pick-up"), FURGAO("Furgão"),
+	FURGAOPEQUENO("Furgão Pequeno"), SUV("SUV");
 	
+	private String categoria;
 
+	/*Construtor*/
+	/**
+	 * @param categoria
+	 */
+	private CategoriasCarros(String categoria) {
+		this.categoria = categoria;
+	}
+	
 }
