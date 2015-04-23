bbbb642991b2b6b2d5b8bcb7f8585b93a3bcde48
Carlos Renato <carlos.asantos@al.infnet.edu.br>
2015-04-23 16:59:51 -0300

    Criação
    Enumeração que disponibilizará as categorias dos carros.

---
+++ LPVeiculos/src/model/veiculos/caracteristicas/carro/CategoriasCarros.java
@@ -0,0 +1,12 @@
+package model.veiculos.caracteristicas.carro;
+
+/**
+ * Nesta enumeração está contida todas as categorias dos carros disponibilizados pela loja.
+ * @author Carlos Renato
+ * @version 23-04-2015
+ *
+ */
+public enum CategoriasCarros {
+	
+
+}
