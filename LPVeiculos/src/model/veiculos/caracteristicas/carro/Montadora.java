6aadbdcc4c2f821744354fc9a737542c9e3a3822
Carlos Renato <carlos.asantos@al.infnet.edu.br>
2015-04-23 20:25:31 -0300

    Criação

---
+++ LPVeiculos/src/model/veiculos/caracteristicas/carro/Montadora.java
@@ -0,0 +1,15 @@
+package model.veiculos.caracteristicas.carro;
+
+/**
+ * Nesta enumeração está contida todas o nome de todas as montadoras de carros
+ * que servem a loja.
+ * 
+ * @author Carlos Renato
+ * @version 23-04-2015
+ *
+ */
+public enum Montadora {
+	
+	
+
+}
