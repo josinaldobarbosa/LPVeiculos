1b734013c02a68ec337becd3204f3051fd9de944
Carlos Renato <carlos.asantos@al.infnet.edu.br>
2015-04-23 19:55:50 -0300

    Formatação

--- LPVeiculos/src/model/veiculos/caracteristicas/carro/Categorias.java
+++ LPVeiculos/src/model/veiculos/caracteristicas/carro/Categorias.java
@@ -1,22 +1,25 @@
 package model.veiculos.caracteristicas.carro;
 
 /**
- * Nesta enumeração está contida todas as categorias dos carros disponibilizados pela loja.
+ * Nesta enumeração está contida todas as categorias dos carros disponibilizados
+ * pela loja.
+ * 
  * @author Carlos Renato
  * @version 23-04-2015
  *
  */
 public enum Categorias {
-	ENTRADA("Entrada"), HATCHPEQUENO("Hatch Pequeno"), HATCHMEDIO ("Hatch Médio"),
-	SEDANPEQUENO("Sedan Pequeno"), SEDANCOMPACTO ("Sedan Compacto"), SEDANMEDIO("Sedan Médio"),
-	SEDANGRANDE ("Sedan Grande"), SWMEDIO("SW Médio"), SWGRANDE ("SW Grande"),
-	MONOCAB ("Monocab"), GRANDCAB ("Grandcab"), SPORT("Sport"),
-	PICKUPPEPEQUENA("Pick-up Pequena"), PICKUPPEGRANDE("Pick-up"), FURGAO("Furgão"),
-	FURGAOPEQUENO("Furgão Pequeno"), SUV("SUV");
-	
+	ENTRADA("Entrada"), HATCHPEQUENO("Hatch Pequeno"), HATCHMEDIO("Hatch Médio"), SEDANPEQUENO(
+			"Sedan Pequeno"), SEDANCOMPACTO("Sedan Compacto"), SEDANMEDIO(
+			"Sedan Médio"), SEDANGRANDE("Sedan Grande"), SWMEDIO("SW Médio"), SWGRANDE(
+			"SW Grande"), MONOCAB("Monocab"), GRANDCAB("Grandcab"), SPORT(
+			"Sport"), PICKUPPEPEQUENA("Pick-up Pequena"), PICKUPPEGRANDE(
+			"Pick-up"), FURGAO("Furgão"), FURGAOPEQUENO("Furgão Pequeno"), SUV(
+			"SUV");
+
 	private String categoria;
 
-	/*Construtor*/
+	/* Construtor */
 	/**
 	 * @param categoria
 	 */
