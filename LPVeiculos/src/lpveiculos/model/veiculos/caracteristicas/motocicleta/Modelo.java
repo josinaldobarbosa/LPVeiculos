e5e003078599ad6d46ee32c36bf8612f26b2a7d3
wallacejsv <wallacejsv@gmail.com>
2015-04-25 01:30:49 -0300

    Criei modelo de motocicleta

---
+++ LPVeiculos/src/lpveiculos/model/veiculos/caracteristicas/motocicleta/Modelo.java
@@ -0,0 +1,13 @@
+package lpveiculos.model.veiculos.caracteristicas.motocicleta;
+
+/**
+ * Nesta enumeração está contida os modelos de motocicletas
+ * 
+ * @author Wallace Jonathan
+ * @version 25-04-2015
+ *
+ */
+
+public enum Modelo {
+
+}
