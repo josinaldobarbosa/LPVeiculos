e8107e105e3fa655990a5ebb0fd12290a4462ced
Josinaldo Albuquerque Barbosa <josinaldo.barbosa@al.infnet.edu.br>
2015-06-09 02:07:36 -0300

    Criando pacote e classe para excecao generica.

---
+++ LPVeiculos/src/lpveiculos/controller/excecoes/Excecao.java
@@ -0,0 +1,10 @@
+package lpveiculos.controller.excecoes;
+
+public class Excecao extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public Excecao(String msg) {
+		super(msg);
+	}
+
+}
